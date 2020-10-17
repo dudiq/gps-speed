ba7df47bbd18591a6332370042894773d3fff969
dudiq <idudiq@gmail.com>
2020-10-17 20:50:48 +0300

    update render and interval

--- gps-speed.ino
+++ gps-speed.ino
@@ -32,11 +32,53 @@ unsigned long last = 0UL;
 
 SSD1306AsciiWire oled;
 
-void setup()
-{
-  Serial.begin(115200);
-  ss.begin(GPSBaud);
-  Wire.begin();
+int fontWidth = 0;
+
+unsigned int DEFAULT_TIME = 200;
+
+int cnt = 0;
+int dx = 10;
+unsigned oldSpeedInt = 0;
+
+void updateSpeed() {
+    oled.set2X();
+    oled.setCursor(0, 0);
+    oled.print("    ");
+    //oled.clearToEOL();
+
+    cnt = cnt + dx;
+    if (cnt > 180 + dx) dx = -10;
+    if (cnt < -dx) dx = 10;
+
+    int speedInt = int(gps.speed.kmph());
+    if (speedInt == 0 || speedInt == oldSpeedInt) return;
+    oldSpeedInt = speedInt;
+    String speed = String(speedInt);
+    int len = speed.length();
+    int newX = (5 * fontWidth - (len * fontWidth));
+
+    oled.setCursor(newX, 0);
+    oled.print(speed);
+
+//    oled.set1X();
+//    oled.setCursor(50, 1);   
+//    oled.print("km/h");
+}
+
+void updateSatellites() {
+    if (!gps.satellites.isUpdated()) return;
+    oled.set1X();
+    oled.setCursor(1, 3);
+    oled.clearToEOL();
+    oled.print("sat: " + String(gps.satellites.value()));
+}
+
+void setup() {
+    delay(4000);
+    
+    Serial.begin(115200);
+    ss.begin(GPSBaud);
+    Wire.begin();
     Wire.setClock(400000L);
 
 #if RST_PIN >= 0
@@ -45,23 +87,27 @@ void setup()
     oled.begin(&Adafruit128x32, I2C_ADDRESS);
 #endif // RST_PIN >= 0
     oled.setFont(font5x7);
-    oled.setCursor(10, 2);
-    oled.println("GPS");
+    fontWidth = oled.fontWidth();
+
+    oled.setCursor(20, 1);
+    oled.print("GPS speedometer");
     delay(4000);
     oled.set2X();
     oled.clear();
+
+    oled.set1X();
+    oled.setCursor(50, 1);
+    oled.print("km/h");
 }
 
-void loop()
-{
-  // Dispatch incoming characters
-  while (ss.available() > 0)
-    gps.encode(ss.read());
+void loop() {
 
-  if (millis() - last > 5000)
-  {
-    last = millis();
-    oled.setCursor(2, 1);   
-    oled.println(String(gps.speed.kmph()) + " km/h");
-  }
+    // Dispatch incoming characters
+
+    while (ss.available() > 0)
+        gps.encode(ss.read());
+
+    updateSpeed();
+    updateSatellites();
+    delay(DEFAULT_TIME);
 }
